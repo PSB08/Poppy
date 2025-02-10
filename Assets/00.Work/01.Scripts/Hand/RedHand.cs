a4b0c37995269967d613cb30b17874fd86e57e2d
PSB08 <Parkseongbin08@gmail.com>
2025-02-10 19:30:12 +0900

    Red hand annotation2

--- Assets/00.Work/01.Scripts/Hand/RedHand.cs
+++ Assets/00.Work/01.Scripts/Hand/RedHand.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(HandFiring))]
 public class RedHand : MonoBehaviour
 {
-    //»¡°£ ¼Õ
+    //»¡°£ ¼Õ ÀûÀÌ¶û ´êÀ¸¸é Àû ÆÄ±«
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
